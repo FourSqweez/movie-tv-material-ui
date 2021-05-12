cb15e4e6b74753999c62bb006c9c260761591ca8
FourSqweez <four7518@gmail.com>
2021-05-12 22:36:00 +0700

    ADD : get data from movies api

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,9 +1,46 @@
-import { Container } from "@material-ui/core";
+import { Container } from '@material-ui/core'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import SingleContent from '../components/SingleContent'
 
 export default function Trending() {
-    return (
-        <Container>
-            trending
-        </Container>
-    )
+	const [contents, setContents] = useState([])
+
+	const fetchTrending = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIES_API_KEY}`
+		)
+		console.log('data', data)
+
+		setContents(data.results)
+	}
+
+	useEffect(() => {
+		fetchTrending()
+	}, [])
+
+	return (
+		<Container>
+			<PageTitle>trending</PageTitle>
+			<TrendingContainer>
+				{contents &&
+					contents.map((c) => (
+						<SingleContent
+							key={c.id}
+							id={c.id}
+							poster={c.poster_path}
+							title={c.title || c.name}
+							date={c.first_air_date}
+							media_type={c.media_type}
+							vote_average={c.vote_average}
+						/>
+					))}
+			</TrendingContainer>
+		</Container>
+	)
 }
+
+const PageTitle = styled.span``
+
+const TrendingContainer = styled.div``
