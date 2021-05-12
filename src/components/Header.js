8c33384a4256982bb9f268f729977e5b1270332b
FourSqweez <four7518@gmail.com>
2021-05-12 17:18:50 +0700

    ADD : Create Header

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+export default function Header() {
+	return (
+		<HeaderContainer>
+			<span>🎬 Entertainment hub 🎥</span>
+		</HeaderContainer>
+	)
+}
+
+const HeaderContainer = styled.div`
+	width: 100%;
+	height: 70px;
+	position: fixed;
+	display: flex;
+	background-color: #39445a;
+	box-shadow: 0px 1px 5px black;
+	z-index: 100;
+	justify-content: center;
+
+	span {
+		color: white;
+		text-transform: uppercase;
+		cursor: pointer;
+		font-size: 3rem;
+		font-family: 'Montserrat', sans-serif;
+		letter-spacing: 1px;
+	}
+
+	@media (max-width: 780px) {
+		align-items: center;
+		span {
+			font-size: 1.8rem;
+		}
+	}
+`
