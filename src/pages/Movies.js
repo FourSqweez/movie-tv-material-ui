4dcee6f01d3b6883e3d117e5007dcd7a5b5e55ec
FourSqweez <four7518@gmail.com>
2021-05-12 17:20:04 +0700

    ADD : Create Pages

---
+++ src/pages/Movies.js
@@ -0,0 +1,10 @@
+import { Container } from '@material-ui/core'
+export default function Movies() {
+	return (
+		<div>
+			<Container>
+				<h3>Movies</h3>
+			</Container>
+		</div>
+	)
+}
---
+++ src/pages/Search.js
@@ -0,0 +1,12 @@
+
+import { Container } from '@material-ui/core'
+
+export default function Search() {
+	return (
+		<Container>
+				<h3>Search</h3>
+		</Container>
+	)
+}
+
+
