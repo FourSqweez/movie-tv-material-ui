3f4b0569c49a73fcb963bc7b2ff71ac34a6401a5
FourSqweez <four7518@gmail.com>
2021-05-12 17:18:28 +0700

    ADD : Create Route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Trending from './pages/Trending'
+import Movies from './pages/Movies'
+import Series from './pages/Series'
+import Search from './pages/Search'
+import SimpleBottomNavigation from './components/MainNav'
+import { Container } from '@material-ui/core'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Header />
+			<div className="app">
+				<Container>
+					<Switch>
+						<Route path="/" exact component={Trending} />
+						<Route path="/movies" component={Movies} />
+						<Route path="/series" component={Series} />
+						<Route path="/search" component={Search} />
+					</Switch>
+				</Container>
+				<SimpleBottomNavigation />
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
