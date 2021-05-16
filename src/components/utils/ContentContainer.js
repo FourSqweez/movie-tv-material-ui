336fa555c9bb602ac71efbbc2549b3d884212558
FourSqweez <four7518@gmail.com>
2021-05-16 16:10:10 +0700

    ADD : utils component

--- src/components/utils/ContentContainer.js
+++ src/components/utils/ContentContainer.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
-export  const ContentContainer = styled.div`
+export const ContentContainer = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: space-evenly;
+`
+
+export const ChipContainer = styled.div`
+	padding: 6px 0;
+	display: flex;
+	justify-content: flex-start;
+	flex-wrap: wrap;
+
+	div {
+		margin: 2px;
+	}
 `
